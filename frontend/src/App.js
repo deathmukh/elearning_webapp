6509479ce69b1ac7fa93485331b57837aa45a7df
alphanoobie <hussaingadi.hg@gmail.com>
2022-06-02 12:27:33 +0530

    sample components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css';
+import Samplecard from './components/samplecard';
 
 function App() {
   return (
     <div className="App">
-      Dumb Bitch Ishani      
+      Dumb Bitch Ishani    
+      <Samplecard/>    
     </div>
   );
 }
